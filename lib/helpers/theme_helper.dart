79bf5c0724b87ceafd58f8102cda10ee01b2a40f
Ismail Tayeb <tayebismail099@gmail.com>
2023-05-19 01:55:49 +0300

    Added Edits to darkColorScheme

--- lib/helpers/theme_helper.dart
+++ lib/helpers/theme_helper.dart
@@ -3,7 +3,9 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'fonts_helper.dart';
 
-class ThemesHelper {
+abstract class ThemesHelper {
+  const ThemesHelper();
+
   static ThemeData lightTheme = ThemeData(
     fontFamily: FontsHelper.fontFamily,
     scaffoldBackgroundColor: const Color(0xFFF5F6F8),
@@ -123,8 +125,124 @@ class ThemesHelper {
     ),
   );
 
-  static ThemeData darkTheme =
-      ThemeData(useMaterial3: true, colorScheme: darkColorScheme);
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: FontsHelper.fontFamily,
+    scaffoldBackgroundColor: const Color(0xFF303030),
+    splashColor: Colors.transparent,
+    colorScheme: darkColorScheme,
+    textTheme: TextTheme(
+      //#region Display
+      displayLarge: TextStyle(
+        fontWeight: FontWeight.w700,
+        fontSize: 57.sp,
+        color: darkColorScheme.primary,
+        letterSpacing: 0.sp,
+      ),
+      displayMedium: TextStyle(
+        fontWeight: FontWeight.w700,
+        fontSize: 45.sp,
+        color: darkColorScheme.primary,
+        letterSpacing: 0.sp,
+        height: (52 / 45),
+      ),
+      displaySmall: TextStyle(
+        fontWeight: FontWeight.w500,
+        fontSize: 36.sp,
+        color: darkColorScheme.primary,
+        letterSpacing: 0.sp,
+      ),
+      //#endregion
+
+      //#region Headline
+      headlineLarge: TextStyle(
+        fontWeight: FontWeight.w700,
+        fontSize: 32.sp,
+        color: darkColorScheme.primary,
+        letterSpacing: 0.sp,
+      ),
+      headlineMedium: TextStyle(
+        fontWeight: FontWeight.w700,
+        fontSize: 28.sp,
+        color: darkColorScheme.primary,
+        letterSpacing: 0.sp,
+      ),
+      headlineSmall: TextStyle(
+        fontWeight: FontWeight.w500,
+        fontSize: 24.sp,
+        color: darkColorScheme.primary,
+        letterSpacing: 0.sp,
+      ),
+      //#endregion
+
+      //#region Title
+      titleLarge: TextStyle(
+        fontWeight: FontWeight.w700,
+        fontSize: 22.sp,
+        color: darkColorScheme.primary,
+        letterSpacing: 0.sp,
+      ),
+      titleMedium: TextStyle(
+        fontWeight: FontWeight.w700,
+        fontSize: 16.sp,
+        color: darkColorScheme.onSurfaceVariant,
+        letterSpacing: 0.15.sp,
+      ),
+      titleSmall: TextStyle(
+        fontWeight: FontWeight.w500,
+        fontSize: 14.sp,
+        color: darkColorScheme.onSurfaceVariant,
+        letterSpacing: 0.1.sp,
+      ),
+      //#endregion
+
+      //#region Label
+      labelLarge: TextStyle(
+        fontWeight: FontWeight.w700,
+        fontSize: 14.sp,
+        color: darkColorScheme.onSurfaceVariant,
+        letterSpacing: 0.1.sp,
+      ),
+
+      labelMedium: TextStyle(
+        fontWeight: FontWeight.w700,
+        fontSize: 12.sp,
+        color: darkColorScheme.onSurfaceVariant,
+        letterSpacing: 0.5.sp,
+      ),
+
+      labelSmall: TextStyle(
+        fontWeight: FontWeight.w700,
+        fontSize: 11.sp,
+        color: darkColorScheme.onSurfaceVariant,
+        letterSpacing: 0.5.sp,
+      ),
+      //#endregion
+
+      //#region Body
+      bodyLarge: TextStyle(
+        fontWeight: FontWeight.w500,
+        fontSize: 16.sp,
+        color: darkColorScheme.onSurfaceVariant,
+        letterSpacing: 0.15.sp,
+      ),
+
+      bodyMedium: TextStyle(
+        fontWeight: FontWeight.w500,
+        fontSize: 14.sp,
+        color: darkColorScheme.onSurfaceVariant,
+        letterSpacing: 0.25.sp,
+      ),
+
+      bodySmall: TextStyle(
+        fontWeight: FontWeight.w500,
+        fontSize: 12.sp,
+        color: darkColorScheme.onSurfaceVariant,
+        letterSpacing: 0.4.sp,
+      ),
+      //#endregion
+    ),
+  );
 }
 
 const lightColorScheme = ColorScheme(
@@ -164,26 +282,28 @@ const lightColorScheme = ColorScheme(
 
 const darkColorScheme = ColorScheme(
   brightness: Brightness.dark,
-  primary: Color(0xFFADC6FF),
-  onPrimary: Color(0xFF002E69),
-  primaryContainer: Color(0xFF004493),
+  primary: Color(0xFF7F9ED9),
+  onPrimary: Color(0xFF000000),
+  primaryContainer: Color(0xFF265CB1),
+  secondary: Color(0xFFFFC266),
+  onSecondary: Color(0xFF000000),
+  secondaryContainer: Color(0xFF8D4F00),
+  background: Color(0xFF1B1B1F),
+  onBackground: Color(0xFFFFFFFF),
+  surface: Color(0xFF303033),
+  onSurface: Color(0xFFFFFFFF),
+  error: Colors.red,
+  onError: Color(0xFFFFFFFF),
+
+  //Older Colors
   onPrimaryContainer: Color(0xFFD8E2FF),
-  secondary: Color(0xFFFFB876),
-  onSecondary: Color(0xFF4B2800),
-  secondaryContainer: Color(0xFF6B3B00),
   onSecondaryContainer: Color(0xFFFFDCC0),
   tertiary: Color(0xFF56D6F6),
   onTertiary: Color(0xFF003641),
   tertiaryContainer: Color(0xFF004E5D),
   onTertiaryContainer: Color(0xFFAFECFF),
-  error: Color(0xFFFFB4AB),
   errorContainer: Color(0xFF93000A),
-  onError: Color(0xFF690005),
   onErrorContainer: Color(0xFFFFDAD6),
-  background: Color(0xFF1B1B1F),
-  onBackground: Color(0xFFE3E2E6),
-  surface: Color(0xFF1B1B1F),
-  onSurface: Color(0xFFE3E2E6),
   surfaceVariant: Color(0xFF44474F),
   onSurfaceVariant: Color(0xFFC4C6D0),
   outline: Color(0xFF8E9099),
